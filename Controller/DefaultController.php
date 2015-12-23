7eb8debf0a5017f3afc2a97c23568a8340239f99
Joseph Zhao <zhaoshuilin2004@yahoo.ca>
2015-12-23 00:54:41 -0500

    missing default controller

---
+++ Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Map2u\CoreBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use \TCPDF;
+
+/**
+ * Map2u Core Default controller.
+ *
+ * @Route("/")
+ */
+class DefaultController extends Controller {
+
+
+
+}
