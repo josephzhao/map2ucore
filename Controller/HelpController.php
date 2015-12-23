0514b39094124e5d1156086702a0d4c52a9e468f
Joseph Zhao <zhaoshuilin2004@yahoo.ca>
2015-12-23 00:58:56 -0500

    missing help controller

---
+++ Controller/HelpController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Map2u\CoreBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Help controller.
+ *
+ * @Route("/help")
+ */
+class HelpController extends Controller {
+
+ 
+}
